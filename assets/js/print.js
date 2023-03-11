219d5a286b7a8f1980140fae74e6063abe377b70
Lara <44525641+LANDSLara@users.noreply.github.com>
2023-03-11 19:53:09 +0100

    Css adjustments

--- assets/js/print.js
+++ assets/js/print.js
@@ -1,18 +1,18 @@
-// function rotateImg() {
+function rotateImg() {
 
-//   const images = document.querySelectorAll("figure");
-//   const minRotation = -9;
-//   const maxRotation = 9;
-//   const minMargin = -6;
-//   const maxMargin= 2;
+  const images = document.querySelectorAll("figure");
+  const minRotation = -6;
+  const maxRotation = 6;
+  const minMargin = -6;
+  const maxMargin= 2;
 
-//   images.forEach((image) => {
-//         const randomRotation = Math.floor(Math.random() * (maxRotation - minRotation + 1) + minRotation);
-//         const randomMargin = Math.floor(Math.random() * (maxMargin - minMargin + 1) + minMargin);
-//         image.style.transform = `rotate(${randomRotation}deg)`;
-//         image.style.marginRight = `${randomMargin}vw`;
-//   });
+  images.forEach((image) => {
+        const randomRotation = Math.floor(Math.random() * (maxRotation - minRotation + 1) + minRotation);
+        const randomMargin = Math.floor(Math.random() * (maxMargin - minMargin + 1) + minMargin);
+        image.style.transform = `rotate(${randomRotation}deg)`;
+  
+  });
 
-// }
+}
 
-// rotateImg();
+rotateImg();
